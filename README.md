4f7f3e15c08f85d5e44c52e70744e600358cf214
Bianca Sanches <biancasanches.dev@gmail.com>
2024-07-04 12:36:32 -0300

    feat: curso e mentoria

---
+++ README.md
@@ -0,0 +1,6 @@
+# Desafio POO
+## Descrição
+Desafio proposto no bootcamp Java da dio visando a abstração do domínio de um bootcamp.
+
+## Objetivo
+O objetivo do desafio é criar um sistema que represente um bootcamp, onde é possível adicionar instrutores, alunos e matérias.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descricao curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JS");
+        curso2.setDescricao("descricao curso Js");
+        curso2. setCargaHoraria(6);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria carreira");
+        mentoria1.setDescricao("descricao mentoria carreira");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
